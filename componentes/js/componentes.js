fe86c8fa26d9eea121146f45254a09661ac5b7da
Daniel Luiz de Souza Gomes <danielgomes6051@gmail.com>
2025-11-17 15:30:33 -0300

    componentes js

--- componentes/js/componentes.js
+++ componentes/js/componentes.js
@@ -1,1 +1,29 @@
+<script>
+        const botao = document.querySelector("#btn-entrar");
+        const titulo = document.querySelector("#btn-entrar");
 
+        botao.onclick = function() {
+            titulo.innerHTML = "Entrando";
+            titulo.style.color = "white";
+            document.body.style.backgroundColor = "black"
+            window.location.href='servicos.html'
+
+        };
+
+        const campo = document.querySelector("#usuario");
+        const mensagem = document.querySelector("#mensagem");
+
+        campo.oninput = function() {
+            mensagem.innerHTML = "Bem-vindo de volta, " + campo.value + "!";
+        }
+        const imagem = document.querySelector("#fotoDestaque");
+
+      imagem.onmouseover = function() {
+          imagem.style.opacity = "0.6";
+      }
+
+      imagem.onmouseout = function() {
+          imagem.style.opacity = "1";
+      }
+
+    </script>
